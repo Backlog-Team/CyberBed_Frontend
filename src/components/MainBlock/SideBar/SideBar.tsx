fbb81316c397059edb6782dc8dc5e3cb918ad00d
themilchenko <ivan.milchenko.92@mail.ru>
2023-10-23 20:49:43 +0300

    BCKLG-1: Add more components

--- src/components/MainBlock/SideBar/SideBar.tsx
+++ src/components/MainBlock/SideBar/SideBar.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Navigation } from "./Navigation/Navigation";
+import { User } from "./User/User";
 import './SideBar.css'
 
 export const SideBar: React.FC = () => {
   return (
     <aside className="sidebar">
-      <section className="user">
-        <div className="userNickname">
-          <h3>Username</h3>
-        </div>
+      <section className="sidebarTop">
+        <User />
+        <Navigation />
+      </section>
+
+      <section className="sidebarBottom">
+        <button>Exit</button>
       </section>
     </aside>
   );
