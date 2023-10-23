d2683ee68395ec89e59043917205f299b7ce0332
themilchenko <ivan.milchenko.92@mail.ru>
2023-10-23 18:22:41 +0300

    BCKLG-1: Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { MainBlock } from './components/MainBlock/MainBlock';
+// import { LoginPage } from './pages/LoginPage/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainBlock />
+    </div>
+  );
+}
+
+export default App;
